de64ec0cc9c3e0a2e6c27561b7b822787d46e163
Andar Manik <59768374+AndarManik@users.noreply.github.com>
2024-11-14 20:06:24 -0500

    first commit

---
+++ backlinkbuilder.ts
@@ -0,0 +1,86 @@
+import axios from "axios";
+
+class BackLinks {
+  idListMap: Map<string, number[]> = new Map();
+
+  has(pageName: string) {
+    return this.idListMap.has(pageName);
+  }
+
+  getRandomId(pageTitle: string) {
+    const maybeIdList = this.idListMap.get(pageTitle);
+    if (!maybeIdList) return [];
+    const randomIndex = Math.floor(Math.random() * maybeIdList.length);
+    return maybeIdList[randomIndex];
+  }
+
+  async buildIdList(pageTitle: string) {
+    const ids: number[] = [];
+    this.idListMap.set(pageTitle, ids);
+
+    const backlinkCount = await getBacklinkCount(pageTitle);
+
+    console.log(backlinkCount);
+
+    const start = await getBacklinks(pageTitle);
+    let continueKey = start.continueKey;
+    ids.push(...start.ids);
+
+    while (continueKey) {
+      const data = await getBacklinks(pageTitle, continueKey);
+      continueKey = data.continueKey;
+      ids.push(...data.ids);
+
+      console.log(
+        `${pageTitle}: ${
+          Math.floor((ids.length / backlinkCount) * 1000) / 10
+        }% complete `
+      );
+    }
+
+    console.log(`${pageTitle} finished with ${ids.length} links`);
+  }
+
+  async isValidPage(pageTitle: string) {
+    return (await getBacklinkCount(pageTitle)) != -1;
+  }
+}
+
+async function getBacklinkCount(pageTitle: string) {
+  const url = `https://linkcount.toolforge.org/api/?page=${pageTitle}&namespaces=0`;
+
+  try {
+    const response = await axios.get(url);
+
+    return response.data.wikilinks.all || -1;
+  } catch (error) {
+    console.error("Error fetching backlink counter:", error);
+    return 0;
+  }
+}
+
+async function getBacklinks(
+  pageTitle: string,
+  blcontinue: number | null = null
+) {
+  const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=backlinks&bltitle=${pageTitle}&blnamespace=0&blfilterredir=nonredirects&blredirect=true&bllimit=500${
+    blcontinue ? `&blcontinue=${blcontinue}` : ""
+  }`;
+
+  try {
+    const response = await axios.get(url);
+    const backlinks = response.data.query.backlinks;
+    const continueKey = response.data.continue
+      ? response.data.continue.blcontinue
+      : null;
+    const ids: number[] = backlinks.map(
+      (link: { pageid: number }) => link.pageid
+    );
+    return { ids, continueKey };
+  } catch (error) {
+    console.error("Error fetching backlinks:", error);
+    return { ids: [], continueKey: null };
+  }
+}
+
+export default BackLinks;
---
+++ server.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import BackLinks from "./backlinkbuilder";
+
+const backLinks = new BackLinks();
+backLinks.buildIdList("Animal");
+backLinks.buildIdList("Painting");
+backLinks.buildIdList("Skyscraper");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client.html");
+});
+
+app.get("/re/:pageName", (req, res) => {
+  const id = backLinks.getRandomId(req.params.pageName);
+  res.redirect(`https://en.wikipedia.org/?curid=${id}`);
+});
+
+app.get("/nu/:pageName", async (req, res) => {
+  if (!(await backLinks.isValidPage(req.params.pageName))) {
+    res.send(`We couldn't find ${decodeURIComponent(req.params.pageName)}`);
+    return;
+  }
+
+  if (!backLinks.has(req.params.pageName))
+    backLinks.buildIdList(req.params.pageName);
+
+  res.send(`localhost:3000/re/${req.params.pageName}`);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000 - http://localhost:3000/");
+});
